a1d9c2a307adc6f8dbc765467d2e10a6f7790580
wandering-kiwi <67548758+wandering-kiwi@users.noreply.github.com>
2024-08-25 13:29:38 +0100

    Add files via upload

--- model.py
+++ model.py
@@ -2,25 +2,44 @@ from timeit import default_timer as timer
 import data
 import os
 
-print('import tensorflow')
+print('importing tensorflow')
 start = timer()
 import tensorflow as tf
+keras=tf.keras
 
 end = timer()
-print('Elapsed time: ' + str(end - start))
+print('')
+print('tensorflow imported: ' + str(end - start))
+
 # get data in form of x_train, x_test, y_train, y_test
-print('load cached data')
+print('loading cached data')
 start = timer()
 x_train, y_train, x_test, y_test = data.load_data(data.path)
-
 end = timer()
-print('Elapsed time: ' + str(end - start))
+print('data loaded: ' + str(end - start))
 
-print(x_train[0].shape)
-# keras=tf.keras
-# model = keras.Sequential([
-#     keras.layers.Input(shape=(640, 640, 3)),
-#     keras.layers.Flatten(),
-#     keras.layers.Dense(128, activation=tf.nn.relu),
-#     keras.layers.Dense(2, activation=tf.nn.softmax)
-# ])
+
+start = timer()
+print('creating model')
+
+model = tf.keras.Sequential(
+    [
+    tf.keras.layers.Input(shape=(640, 640, 3)),
+    tf.keras.layers.BatchNormalization(),
+
+    tf.keras.layers.Conv2D(32, (3,3), padding='same', activation="relu"),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=2),
+
+    tf.keras.layers.Conv2D(64, (3,3), padding='same', activation="relu"),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=2),
+
+    tf.keras.layers.Flatten(),
+
+    tf.keras.layers.Dense(512, activation="relu"),
+    tf.keras.layers.Dense(64, activation="relu"),
+    tf.keras.layers.Dense(2, activation="softmax")
+]
+)
+model.summary()
+end = timer()
+print('model created: ' + str(end - start))
